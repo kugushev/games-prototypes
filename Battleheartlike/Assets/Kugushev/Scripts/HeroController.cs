2c9083b0252c465617bec9cef74272bc13b1388c
Aleksandr Kugushev <kugushew@gmail.com>
2020-12-05 01:43:16 +0300

    add battleheartlike first steps

---
+++ Battleheartlike/Assets/Kugushev/Scripts/HeroController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Kugushev.Scripts
+{
+    public class HeroController : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private XRController xrController;
+
+        private void FixedUpdate()
+        {
+            if (xrController.inputDevice.IsPressed(InputHelpers.Button.Trigger, out bool isPressed) && isPressed)
+            {
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out var hit,
+                    Mathf.Infinity))
+                {
+                    agent.destination = hit.point;
+                }
+            }
+
+            if (xrController.inputDevice.IsPressed(InputHelpers.Button.Grip, out bool grip) && grip)
+            {
+                agent.enabled = false;
+            }
+            else
+            {
+                agent.enabled = true;
+            }
+        }
+    }
+}
---
+++ Battleheartlike/Assets/Kugushev/Scripts/UnitController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Kugushev.Scripts
+{
+    public class UnitController: MonoBehaviour
+    {
+        public void OnHover(XRBaseInteractor interactor)
+        {
+            transform.Rotate(0, Mathf.PI / 2f, 0);
+        }
+    }
+}
