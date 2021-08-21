bb1fe175359023fc0f0fc78e5cf78eac402b9195
Aleksandr Kugushev <kugushew@gmail.com>
2021-08-21 08:48:30 +0300

    add banchmark scene

---
+++ Moshatar/Assets/Kugushev/Scripts/Presentation/Common/FPSCounter.cs
@@ -0,0 +1,18 @@
+﻿using Kugushev.Scripts.Common.Utils;
+using TMPro;
+using UnityEngine;
+
+namespace Kugushev.Scripts.Presentation.Common
+{
+    public class FPSCounter: MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI text;
+
+        private void Update()
+        {
+            float fps = 1f / Time.unscaledDeltaTime;
+            int fpsInt = Mathf.FloorToInt(fps);
+            text.text = StringBag.FromInt(fpsInt);
+        }
+    }
+}
---
+++ Moshatar/Assets/Kugushev/Scripts/Presentation/PoC/Benchmark/BenchmarkManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using Kugushev.Scripts.Common.Utils;
+using TMPro;
+using Unity.XR.Oculus;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kugushev.Scripts.Presentation.PoC.Benchmark
+{
+    public class BenchmarkManager : MonoBehaviour
+    {
+        [SerializeField] private Button spawn;
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private TextMeshProUGUI units;
+
+        private Vector3 _currentSpawn = Vector3.zero;
+        private int _count = 0;
+
+        private void Awake()
+        {
+            Performance.TryGetAvailableDisplayRefreshRates(out var rates);
+            units.text = string.Join(", ", rates);
+
+            if (rates.Length > 0)
+            {
+                var result = Performance.TrySetDisplayRefreshRate(rates.Max());
+                if (!result) 
+                    units.color = Color.red;
+            }
+            else
+                units.color = Color.magenta;
+
+
+            spawn.onClick.AddListener(SpawnNext);
+        }
+
+        private void OnDestroy()
+        {
+            spawn.onClick.RemoveListener(SpawnNext);
+        }
+
+        private void SpawnNext()
+        {
+            const int spawningUnits = 16;
+            const float unitSize = 2f;
+
+            var spawnPoint = new Vector3(
+                -1 * (spawningUnits / 2) * unitSize,
+                _currentSpawn.y,
+                _currentSpawn.z + unitSize
+            );
+
+            for (int i = 0; i < spawningUnits; i++)
+            {
+                Instantiate(prefab, spawnPoint, Quaternion.Euler(0, 180, 0));
+                spawnPoint.x += unitSize;
+            }
+
+            _currentSpawn = spawnPoint;
+            _count += spawningUnits;
+            units.text = StringBag.FromInt(_count);
+        }
+    }
+}
